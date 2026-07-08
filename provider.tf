3e99277517dd132e0b0a8551a50dd9802955285d
M, Vinay <vinay_m@baxter.com>
2026-07-08 14:19:58 +0530

    chabged code

--- provider.tf
+++ provider.tf
@@ -30,6 +30,6 @@ provider "azurerm" { # This is only required when the User, Service Principal, o
   features {}
   subscription_id = var.ARM_SUBSCRIPTION_ID
   tenant_id = var.ARM_TENANT_ID
-  # client_id = var.ARM_CLIENT_ID
-  # client_secret = var.ARM_CLIENT_SECRET
+  client_id = var.ARM_CLIENT_ID
+  client_secret = var.ARM_CLIENT_SECRET
 }

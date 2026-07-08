428f86f2ff5f4649a69b33a7f53a3928107bc9a8
M, Vinay <vinay_m@baxter.com>
2026-07-08 13:43:36 +0530

    added plan

--- provider.tf
+++ provider.tf
@@ -26,10 +26,10 @@ terraform {
 }
 
 # # Configure the Microsoft Azure Provider
-# provider "azurerm" { # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-#   features {}
-#   subscription_id = var.ARM_SUBSCRIPTION_ID
-#   tenant_id = var.ARM_TENANT_ID
-#   # client_id = var.ARM_CLIENT_ID
-#   # client_secret = var.ARM_CLIENT_SECRET
-# }
+provider "azurerm" { # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  tenant_id = var.ARM_TENANT_ID
+  # client_id = var.ARM_CLIENT_ID
+  # client_secret = var.ARM_CLIENT_SECRET
+}
